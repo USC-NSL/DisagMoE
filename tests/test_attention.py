601ff8a9d32eeebcb4bb01cd2f4c85951a70a714
shaoyuw <wangshao@usc.edu>
2024-10-07 03:40:15 +0000

    test chunked prefill in attention

--- tests/test_attention.py
+++ tests/test_attention.py
@@ -80,16 +80,40 @@ def make_naive_mapping(lens, mode):
         elif mode == "decode":
             end_slot = start * block_size + l - 1
             slots_table.append(end_slot)
-        elif mode == "mix":
-            # chunked prefill
-            pass
         else:
             assert False
             
     block_table = pad_sequence(block_table, batch_first=True, padding_value=0)
     slots_table = torch.tensor(slots_table, dtype=torch.long)
     return block_table, slots_table
+
+def make_mix_mapping(seq_lens, query_lens, num_prefill, num_decode):
+    # tail query of seqs
+    assert len(seq_lens) == num_prefill + num_decode
+    block_table = []
+    slots_table = []
+    allocated_blocks = 0
     
+    for i, l in enumerate(seq_lens):
+        num_blocks = (l + block_size) // block_size
+        start = allocated_blocks
+        end = num_blocks + allocated_blocks
+        block_list = list(range(start, end))
+        
+        block_table.append(torch.tensor(block_list, dtype=torch.int32))
+        if i < num_prefill:
+            start_slot = start * block_size
+            end_slot = start_slot + l
+            slot_list = list(range(end_slot - query_lens[i], end_slot))
+            slots_table.extend(slot_list)
+        else:
+            end_slot = start * block_size + l - 1
+            slots_table.append(end_slot)
+        
+    block_table = pad_sequence(block_table, batch_first=True, padding_value=0)
+    slots_table = torch.tensor(slots_table, dtype=torch.long)
+    return block_table, slots_table
+
 def test_prefill():
     num_prefills = 8
     lens = [random.randint(32, b=127) for _ in range(num_prefills)]
@@ -155,8 +179,40 @@ def test_decode():
     
 
 def test_prefill_decode():
-    pass
-
+    num_decode_tokens = 8
+    num_prefills = 8
+    lens = [random.randint(64, b=127) for _ in range(num_decode_tokens + num_prefills)]
+    seqlens = torch.tensor(lens, dtype=torch.int32, device=torch.get_default_device())
+    query_lens = [random.randint(20, 50) for _ in range(num_prefills)]
+    max_query_len = max(query_lens)
+    num_prefill_tokens = sum(query_lens)
+    max_prefill_seq_len = max(lens[:num_prefills])
+    max_decode_seq_len = max(lens[num_prefills:])
+    query_start_loc = make_seqlens(query_lens)
+    seq_start_loc = make_seqlens(lens)
+    block_table, slot_mapping = make_mix_mapping(lens, query_lens, num_prefills, num_decode_tokens)
+    context_lens_tensor = [lens[i] - query_lens[i] for i in range(num_prefills)] + [lens[i + num_prefills] - 1 for i in range(num_decode_tokens)]
+    meta = FlashAttentionMetadata(
+        num_prefills=num_prefills,
+        num_prefill_tokens=num_prefill_tokens,
+        num_decode_tokens=num_decode_tokens,
+        slot_mapping=slot_mapping,
+        seq_lens=lens,
+        seq_lens_tensor=seqlens,
+        max_query_len=max_query_len,
+        max_prefill_seq_len=max_prefill_seq_len,
+        max_decode_seq_len=max_decode_seq_len,
+        query_start_loc=query_start_loc,
+        seq_start_loc=seq_start_loc,
+        context_lens_tensor=context_lens_tensor,
+        block_tables=block_table,
+        use_cuda_graph=False,
+    )
+    inputs = torch.randn((num_decode_tokens + num_prefill_tokens, hidden_size))
+    positions = torch.zeros_like(inputs, dtype=torch.long)
+    attn.forward(positions, inputs, cache, meta)
+    print(f">>> chunked_prefill test passed")
+    
 test_prefill()
 
 test_decode()
