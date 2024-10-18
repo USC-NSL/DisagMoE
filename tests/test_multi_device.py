9967f8e35f5b7eda88df777b4732e1f58eb52ff6
hogura <heguangrongdd@qq.com>
2024-10-18 07:11:36 +0000

    Add multi device test

---
+++ tests/test_multi_device.py
@@ -0,0 +1,49 @@
+from disagmoe.frontend.controller import init_controller
+from disagmoe.utils.placement import ModelPlacement
+from disagmoe.utils.constants import *
+
+import time
+
+master = init_controller(1, 4)
+
+tokenizer = TOKENIZER_DEV_ID
+sampler = SAMPLER_DEV_ID
+
+mp = ModelPlacement(
+    attn = {
+        0: [0, 2],
+        2: [1, 3],
+    },
+    expert = {
+        1: [(0, 0), (2, 0)],
+        3: [(1, 0), (3, 0)],
+    },
+    tokenizer = tokenizer,
+    sampler = sampler,
+)
+
+edges = [
+    (tokenizer, 0),
+    (0, 1),
+    (1, 2),
+    (2, 3),
+    (3, sampler),
+    
+    (sampler, 0),
+    (3, 0),
+]
+
+master.init_engine(mp)
+
+print("engine inited")
+
+master.start_engine()
+
+print("engine started")
+
+time.sleep(2)
+
+master.put_request([1])
+
+while True:
+    pass
