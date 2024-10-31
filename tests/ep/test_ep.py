c1ce2d0e1dd7d33c6763a1d2589d78cffc08e32d
hogura <heguangrongdd@qq.com>
2024-10-31 11:33:55 +0800

    Fixed nccl uid in multiple GPUs

--- tests/ep/test_ep.py
+++ tests/ep/test_ep.py
@@ -9,18 +9,20 @@ import torch
 tokenizer = TOKENIZER_DEV_ID
 sampler = SAMPLER_DEV_ID
 
-cluster_config = ClusterConfig(n_node=1, n_gpu=3, 
+cluster_config = ClusterConfig(n_node=1, n_gpu=6, 
                                id_tokenizer=tokenizer, 
                                id_sampler=sampler)
 
 master = init_controller(cluster_config.n_node, cluster_config.n_gpu)
 
 model_config = duo_expert_mixtral
-model_config.num_layer = 4
+model_config.num_layer = 16
 model_config.ep_size = 2
-model_config.num_experts = 2
+model_config.num_experts = 8
 
 mp = get_model_placement(model_config, cluster_config, "interleave")
+
+print(mp)
 
 cache_config = CacheConfig(BLOCK_SIZE, 0.8, 2, "auto", 
                             num_gpu_blocks=NUM_BLOCKS, 
@@ -36,7 +38,7 @@ master.start_profile()
 
 print("engine started")
 
-n = 8
+n = 32
 
 master.put_multi_request(n)
 
