3891f04f5ab788b6ac67f0b4ac2361de80f2db63
hogura <heguangrongdd@qq.com>
2025-03-14 11:01:04 +0000

    Add gpu-usage; separate args

--- benchmark/benchmark_serving.py
+++ benchmark/benchmark_serving.py
@@ -6,6 +6,7 @@ from disagmoe.utils.constants import *
 from disagmoe.config import ModelConfig, CacheConfig, mixtral_config, SamplingConfig
 from disagmoe.frontend.datatypes import SloStat, TraceContext, SamplerStepInfo
 from benchmark.workload import PoissonGenerator, Workload, UniformGenerator, get_generator
+from benchmark.utils import get_parser_base
 from typing import List, Dict, Tuple
 from argparse import ArgumentParser
 from dataclasses import dataclass, asdict
@@ -120,7 +121,7 @@ def launch(args):
 
     master = init_controller(cluster_config.n_node, cluster_config.n_gpu, args.nsys)
 
-    cache_config = CacheConfig(args.block_size, 0.8, 2, "auto",
+    cache_config = CacheConfig(args.block_size, args.gpu_usage, 2, "auto",
                                num_gpu_blocks=args.num_blocks + RESERVED_BLOCKS if args.num_blocks else None, # default should be None
                                num_reserved_blocks=RESERVED_BLOCKS)
 
@@ -132,6 +133,8 @@ def launch(args):
         master.start_profile(args.profile_dir)
 
     master.start_engine()
+    
+    return master
     
    
 async def process_response(resp: AsyncResult, pbar):
@@ -325,43 +328,7 @@ async def benchmark_serving(args):
                            t_submitted, results)
     
 def get_args():
-    parser = ArgumentParser()
-    
-    parser.add_argument("-r", "--rate", type=float, default=0, help="rate of incoming requests, seconds per request")
-    parser.add_argument("-i", "--input-len", type=int, default=1, help="initial prefill length for each seqeunce")
-    parser.add_argument("-o", "--output-len", type=int, default=32, help="length of output sequence")
-    parser.add_argument("-n", "--num-requests", type=int, default=1000, help="number of requests to generate")
-    parser.add_argument("-p", "--profile-dir", type=str, default=None, help="directory to store torch profiler output")
-    parser.add_argument("-ca", "--cuda-graph-attn", action="store_true", default=False, help="enable cuda graph for attention")
-    parser.add_argument("--serial-gemm", action="store_true", default=False, help="use serial gemm for experts")
-    parser.add_argument("--nsys", action="store_true", help="enable nsys profiling")
-    parser.add_argument("-f", "--file", type=str, default="reports/benchmark.xlsx", help="file to write benchmark results")
-    parser.add_argument("--trace", action="store_true", default=False, help="generate trace")
-    parser.add_argument("--generator-type", type=str, default="poisson", help="generator type, including 'poisson' and 'uniform'.")
-    parser.add_argument("--analyze-throughput", action="store_true", default=False, help="analyze throughput")
-    
-    # model config
-    parser.add_argument("-N", "--num-nodes", type=int, default=1, help="number of nodes")
-    parser.add_argument("-g", "--num-gpus", type=int, default=4, help="number of gpus per node")
-    parser.add_argument("--tp-size", type=int, default=1, help="tensor parallel size")
-    parser.add_argument("--ep-size", type=int, default=2, help="expert parallel size")
-    parser.add_argument("--dp-size", type=int, default=1, help="data parallel size")
-    parser.add_argument("-L", "--num-layers", type=int, default=32, help="number of layers")
-    parser.add_argument("-E", "--num-experts", type=int, default=8, help="number of experts")
-    parser.add_argument("-K", "--topk", type=int, default=1, help="top k")
-    parser.add_argument("--num-blocks", type=int, default=None, help="number of blocks in cache; deprycated due to auto-num-blocks")
-    parser.add_argument("--block-size", type=int, default=BLOCK_SIZE, help="block size in cache")
-    parser.add_argument("--graph-stride", type=int, default=8, help="CUDA graph batch size stride")
-    parser.add_argument("--max-batch-size-attn", type=int, default=256, help="max batch size for attention")
-    parser.add_argument("--max-batch-size-expert", type=int, default=512, help="max batch size for experts")
-    
-    # placement config
-    parser.add_argument("--placement", type=str, default="pipeline", help="placement strategy")
-    parser.add_argument("--zigzag-attn", action="store_true", default=False, help="enable zigzag attention placment")
-    parser.add_argument("--step-attn", type=int, default=2, help="number of steps in attention placement")
-    parser.add_argument("--step-expert", type=int, default=1, help="number of steps in expert placement")
-    
-    args = parser.parse_args()
+    args = get_parser_base().parse_args()
     
     assert args.num_experts >= args.topk, "number of experts must be greater than topk"
     
