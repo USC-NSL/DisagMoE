fecd3fa85a99b48539afbadba1e06c5a9989ba6c
shaoyuw <wangshao@usc.edu>
2025-03-26 00:42:30 +0000

    set queue length heatmap

--- benchmark/plotter/queue_length.py
+++ benchmark/plotter/queue_length.py
@@ -29,7 +29,7 @@ def draw_heatmap(worker_id, data):
     # enumerate queue_length as dict
     queue_length, step_executed_layer, step_start_time_ms = data
     
-    plt.figure(figsize=(10, 2))
+    plt.figure(figsize=(20, 8))
     figure, ax = plt.subplots()
     
     layer_ids = []
@@ -53,17 +53,18 @@ def draw_heatmap(worker_id, data):
     data = np.array([sample_from_mid(queue_length[layer_id], args.steps) for layer_id in layer_ids])
     
     plt.imshow(data, cmap='hot', origin='lower', extent=[0, args.steps, 0, nlayers])
-    plt.xticks(np.arange(args.steps)[ : : 10], step_start_time_ms_sampled[ : : 10], rotation=45)
+    plt.xticks(np.arange(args.steps)[-1 : ], step_start_time_ms_sampled[-1 : ], rotation=0)
     
     # executed_layer_ids = np.argmax(data, axis=0)
     for i, layer_id in enumerate(sample_from_mid(step_executed_layer, args.steps)):
-        plt.plot([i, i+1], [layer_id, layer_id], color='cyan', linestyle='--', linewidth=1)
+        plt.plot([i, i+1], [layer_id, layer_id], color='cyan', linestyle='--', linewidth=2)
     
+    ax.set_aspect(2.5)
     plt.xlabel('time (ms)')
     plt.ylabel('layer')
-    plt.title('queue length per layer)')
-    plt.colorbar(label='Queue Length', orientation='vertical', shrink=0.4)
-    plt.savefig(f'{plot_dir}/{worker_id}.pdf', bbox_inches='tight')
+    plt.title('queue length per layer')
+    plt.colorbar(label='Queue Length', orientation='vertical', shrink=0.5)
+    plt.savefig(f'{plot_dir}/{worker_id}.png', bbox_inches='tight', dpi=300)
     plt.close()
 
 # for each row of df, do draw_plot
