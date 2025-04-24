cc4763e6e986852362852b6e38bfe9758412b873
shaoyuw <wangshao@usc.edu>
2025-04-24 12:18:53 +0000

    update script

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 
-prefix=mqa_top1_
+prefix=mqa_top1
 
-mkdir -p "{$prefix}_short"
+mkdir -p "$prefix"_short
 python benchmark/scripts/benchsuite.py  "short"
-mv $(ls -d mqa_top1/*) "{$prefix}_short"
+mv $(ls -d mqa_top1/*) "$prefix"_short
 
-mkdir -p "{$prefix}_reasonable"
+mkdir -p "$prefix"reasonable
 python benchmark/scripts/benchsuite.py "reasonable"
-mv $(ls -d mqa_top1/*) "{$prefix}_reasonable"
+mv $(ls -d mqa_top1/*) "$prefix"reasonable
 
-mkdir -p "{$prefix}_reasonable_v2"
+mkdir -p "$prefix"reasonable_v2
 python benchmark/scripts/benchsuite.py "reasonable_v2"
-mv $(ls -d mqa_top1/*) "{$prefix}_reasonable_v2"
+mv $(ls -d mqa_top1/*) "$prefix"reasonable_v2
 
