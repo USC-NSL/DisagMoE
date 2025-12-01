7685013a584d149dbff2feb90014ad3e13739f3e
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2025-11-30 19:45:05 -0800

    m_indicies padding should be -1, not 0

--- csrc/cuda_ops/indices_copy_pad.cu
+++ csrc/cuda_ops/indices_copy_pad.cu
@@ -27,7 +27,7 @@ __global__ void copy_and_pad_kernel(
             if (i < num_tokens) {
                 out_m_indices[i] = in_m_indices[i];
             } else {
-                out_m_indices[i] = 0;
+                out_m_indices[i] = -1;
             }
         }
     } else {
