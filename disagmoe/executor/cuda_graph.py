acaddd02f08a957247981f25b977bdfea816138d
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2025-12-06 19:00:33 -0800

    typo

--- disagmoe/executor/cuda_graph.py
+++ disagmoe/executor/cuda_graph.py
@@ -324,13 +324,13 @@ class CUDAGraphExpertsExecutor:
 
                 batch = ExpertForwardBatch(
                             layer_id=layer_id,
-                            data=self.static_input_hiddens,
+                            data=self.static_input_hiddens[:graph_batch_size],
                             num_tokens=graph_batch_size,
                             meta_c=None,
                             proc_func=None,
                             post_proc_func=None,
                             batch_sizes=None,
-                            m_indices=self.static_input_m_indices
+                            m_indices=self.static_input_m_indices[:graph_batch_size]
                         )
                 
                 for _ in range(2):
