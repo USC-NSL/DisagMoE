a215c8a0965c7c7db1e86b2cb54ab2b9d8ff26b9
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2025-12-10 06:34:40 -0800

    log pool sync

--- csrc/muhelper/muhelper.cpp
+++ csrc/muhelper/muhelper.cpp
@@ -604,8 +604,10 @@ void MuPool::run() {
 
         // process the incoming batch and sync the NCCL CUDA streams
         for (auto &p : pending) {
-            this->process_batch(p.tensor, p.meta);
             this->peer_channels[p.peer_id]->sync();
+            if (this->comm_log_.is_open()) {
+                this->comm_log_ << "rank " << this->device_id << " synced channel " << p.peer_id << LEND;
+            this->process_batch(p.tensor, p.meta);
         }
     }
 }
